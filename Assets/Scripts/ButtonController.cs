17e8967b1e061855b0a85fbadd428bb743692533
the-it-weirdo <ddsmegh4@gmail.com>
2019-10-09 18:54:52 +0530

    implements primary UI and marker scan

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	private bool aboutActive = false;
+
+	public Transform PanelAbout;
+	public Transform ImageHi;
+
+	// Use this for initialization
+	void Start () {
+		PanelAbout.gameObject.SetActive (aboutActive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void viewAbout()
+	{
+		if (!aboutActive) {
+			aboutActive = true;
+		} else {
+			aboutActive = false;
+		}
+		ImageHi.gameObject.SetActive (!aboutActive);
+		PanelAbout.gameObject.SetActive (aboutActive);
+	}
+}
